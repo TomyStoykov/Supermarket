0a7bd25675ebb54f989ade6e9269ad2d3dd1789d
Tomi <tomi.vd02@gmail.com>
2023-02-22 20:07:36 +0200

    Test

--- src/OnlineSuperMarket.java
+++ src/OnlineSuperMarket.java
@@ -1,13 +1,27 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class OnlineSuperMarket {
-    List<Product> products;
+    private List<Product> products;
+    List<Product> shoppingCartArr;
+    private double balance;
+    private final String fileName = "db.bin";
 
-    public OnlineSuperMarket(List<Product> products) {
+    public OnlineSuperMarket(double balance) {
+        this.balance = balance;
         products = new ArrayList<>();
+        shoppingCartArr = new ArrayList<>();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public void addProduct(Product product){
@@ -30,5 +44,50 @@ public class OnlineSuperMarket {
             sortedProducts.add(product);
         }
     }
+    }
+    public void shoppingCart(String productName){
+        Product product = findProduct(productName);
+    if(product == null){
+        System.out.println("Product not found");
+    }
+    else{
+        shoppingCartArr.add(product);
+        System.out.println("Product added to the cart:" + product.getProduct());
+        balance = getBalance() - product.getPrice();
+        System.out.println("Remaining balance: " + balance);
+    }
+    }
+    public Product findProduct(String product){
+        for (Product product1 : products){
+            if(product1.getProduct().equals(product)){
+                return product1;
+            }
+        }
+        return null;
+    }
+
+    private void saveData(){
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
+            out.writeObject(products);
+            out.writeDouble(balance);
+            out.close();
+            fileOutputStream.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void loadData(){
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fileInputStream);
+            products = (List<Product>) in.readObject();
+            balance = in.readDouble();
+            in.close();
+            fileInputStream.close();
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
