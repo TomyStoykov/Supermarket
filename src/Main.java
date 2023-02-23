c4725a94b94cab2f8555f6df28fcdb0b6b9e34ac
Tomi <tomi.vd02@gmail.com>
2023-02-23 21:53:44 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
-import java.io.Serializable;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        OnlineSuperMarket onlineSuperMarket = new OnlineSuperMarket(10000);
-        Product p1 = new Product("Milk ", "Dairy ", 2.5);
-        Product p2 = new Product("Eggs ", "Dairy ", 1.5);
-        Product p3 = new Product("Bread ", "Bakery ", 1.0);
-        Product p4 = new Product("Apple ", "Fruit ", 0.5);
-        Product p5 = new Product("Orange ", "Fruit ", 0.8);
-        onlineSuperMarket.addProduct(p1);
-        onlineSuperMarket.addProduct(p2);
-        onlineSuperMarket.addProduct(p3);
-        onlineSuperMarket.addProduct(p4);
-        onlineSuperMarket.addProduct(p5);
+        OnlineSuperMarket onlineSuperMarket = new OnlineSuperMarket(5);
+        Product products[] = {new Product("Milk", "Dairy", 2.5),
+        new Product("Eggs", "Dairy", 1.5),
+        new Product("Bread", "Bakery", 1.0),
+        new Product("Apple", "Fruit", 0.5),
+        new Product("Orange", "Fruit", 0.8)
+        };
+        for(Product product : products) {
+            onlineSuperMarket.addProduct(product);
+        }
         onlineSuperMarket.printProducts();
-        onlineSuperMarket.sortCategory("Dairy");
+        onlineSuperMarket.sortCategory("Fruit");
+        onlineSuperMarket.sortPrices();
+        onlineSuperMarket.shoppingCart("Eggs");
+        onlineSuperMarket.shoppingCart("Bread");
+        onlineSuperMarket.checkout();
     }
 }
--- src/OnlineSuperMarket.java
+++ src/OnlineSuperMarket.java
@@ -1,8 +1,6 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class OnlineSuperMarket implements Serializable {
     private List<Product> products;
@@ -30,24 +28,36 @@ public class OnlineSuperMarket implements Serializable {
     }
 
     public void printProducts(){
-        System.out.println("Printing the list of products: ");
+        //System.out.println("Printing the list of products: ");
         for(Product product : products){
-            System.out.println(product.getProduct() + product.getCategory() + product.getPrice());
+            System.out.println(product.getProduct()+ " " + product.getCategory() +" "+ product.getPrice());
         }
     }
     public void sortPrices(){
-        Collections.sort(products, Comparator.comparing(Product::getPrice));
+        products.sort(Comparator.comparing(Product::getPrice));
+        System.out.println("Prices sorted");
+        printProducts();
     }
     public void sortCategory(String category){
-    List<Product> sortedProducts = new ArrayList<>();
-    for(Product product : products){
-        if(product.getCategory().equals(category)){
-            sortedProducts.add(product);
+        List<Product> sortedProducts = new ArrayList<>();
+
+        for(Product product : products){
+            String currProductCategory = product.getCategory();
+
+            if(currProductCategory.equals(category)){
+                sortedProducts.add(product);
+            }
         }
-    }
-    for(Product sortedProduct : sortedProducts){
-        System.out.println(sortedProduct.getProduct());
-    }
+
+        if(sortedProducts.isEmpty()){
+            System.out.println("No products in that category");
+        }
+        else{
+            System.out.println("Products in category " + category + ": ");
+            for (Product sortedProduct : sortedProducts){
+                System.out.println(sortedProduct.getProduct() +" "+ sortedProduct.getPrice());
+            }
+        }
     }
     public void shoppingCart(String productName){
         Product product = findProduct(productName);
@@ -55,13 +65,13 @@ public class OnlineSuperMarket implements Serializable {
         System.out.println("Product not found");
     }
     else if (getBalance() - product.getPrice() < 0){
-            System.out.println("Insufficient balance");
+            System.out.println("Insufficient balance, " + product.getProduct() + " wasn't added to the cart" );
         }
     else{
         shoppingCartArr.add(product);
         System.out.println("Product added to the cart:" + product.getProduct());
         balance = getBalance() - product.getPrice();
-        System.out.println("Remaining balance: " + balance);
+        System.out.println("Remaining balance: $" + balance);
         saveData();
     }
     }
@@ -98,7 +108,7 @@ public class OnlineSuperMarket implements Serializable {
             e.printStackTrace();
         }
     }
-    private void checkout(){
+    public void checkout(){
         if(shoppingCartArr.isEmpty()){
             System.out.println("There is nothing in the shopping cart");
             return;
@@ -106,22 +116,25 @@ public class OnlineSuperMarket implements Serializable {
         double total = 0;
         for(Product product : shoppingCartArr){
             total += product.getPrice();
-            System.out.println("Product : " + product.getProduct() + " = " + product.getPrice());
+            System.out.println("Product : " + product.getProduct() + " = $" + product.getPrice());
         }
-        System.out.println("Total cost is : " +total);
-        double remainingBalance = getBalance() - total;
-        System.out.println("Money left: " +remainingBalance);
-        if(remainingBalance < 0){
+        System.out.println("Total cost is : $" +total);
+        //double remainingBalance = getBalance() - total;
+        System.out.println("Money left: $" +getBalance());
+        if(getBalance() < 0){
             System.out.println("Insufficient balance");
         }
         try{
+            String pattern = "yyyy-MM-dd HH:mm:ss";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
             String file = "checkout_receipt.txt";
-            FileWriter fileWriter = new FileWriter(file);
+            FileWriter fileWriter = new FileWriter(file,true);
+            fileWriter.write("\nDate: "+simpleDateFormat.format(new Date()) + "\n");
             fileWriter.write("Items:");
             for(Product product : shoppingCartArr){
-                fileWriter.write(product.getProduct() + " = " + product.getPrice() + "\n");
+                fileWriter.write(product.getProduct() + " = $" + product.getPrice() + "\n");
             }
-            fileWriter.write("Total is: "+total);
+            fileWriter.write("Total is: $"+total);
             fileWriter.close();
         }catch (IOException e){
             e.printStackTrace();
