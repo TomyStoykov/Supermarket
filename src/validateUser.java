d898caf6ca9d29c4d3e71ab6a1ca22324b7248ac
Tomi <tomi.vd02@gmail.com>
2023-02-24 15:46:32 +0200

    Test

--- src/validateUser.java
+++ src/validateUser.java
@@ -2,51 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class validateUser {
-    /*private String userName;
-    private String email;
-    private String password;
-    private String bankAccount;
 
-    Scanner scanner = new Scanner(System.in);
-
-    public validateUser(String userName, String email, String password, String bankAccount) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.bankAccount = bankAccount;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBankAccount() {
-        return bankAccount;
-    }
-
-    public void setBankAccount(String bankAccount) {
-        this.bankAccount = bankAccount;
-    }*/
     public  void register(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your username: ");
